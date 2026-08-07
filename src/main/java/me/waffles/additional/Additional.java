e5cfd4fae6c41994ec21db4cf4440bc822b536e2
Waffles3438 <96705793+Waffles3438@users.noreply.github.com>
2026-08-07 00:14:14 -0400

    3.0.6
    something big is happening

--- src/main/java/me/waffles/additional/Additional.java
+++ src/main/java/me/waffles/additional/Additional.java
@@ -1,6 +1,7 @@
 package me.waffles.additional;
 
 import cc.polyfrost.oneconfig.utils.commands.CommandManager;
+import me.waffles.additional.render.NameTagESP;
 import me.waffles.additional.util.BotUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
@@ -35,6 +36,7 @@ public class Additional {
     @Mod.EventHandler
     public void onInit(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new NameTagESP());
         config = new ModConfig();
         duelsStatsList = new EldestRemovalMap<>(ModConfig.maxCacheSize);
         bedwarsStatsList = new EldestRemovalMap<>(ModConfig.maxCacheSize);
